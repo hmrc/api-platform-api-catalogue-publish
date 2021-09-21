bd9673bee88bd0ef9aa46ff1373cd189a9cebbe5
Matt Clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2021-09-21 08:16:37 +0100

    API-5106: commit to share code

--- app/uk/gov/hmrc/apiplatformapicataloguepublish/apidefinition/connector/ApiDefinitionConnector.scala
+++ app/uk/gov/hmrc/apiplatformapicataloguepublish/apidefinition/connector/ApiDefinitionConnector.scala
@@ -20,33 +20,35 @@
 
 import uk.gov.hmrc.apiplatformapicataloguepublish.apidefinition.models.{ApiDefinition, ApiDefinitionJsonFormatters}
 import uk.gov.hmrc.apiplatformapicataloguepublish.apidefinition.connector.ApiDefinitionConnector.Config
+
 import javax.inject.{Inject, Singleton}
 import play.api.Logging
-import uk.gov.hmrc.http.HeaderCarrier
-import uk.gov.hmrc.http.HttpClient
+import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, NotFoundException}
 import uk.gov.hmrc.play.http.ws.WSGet
-
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.control.NonFatal
 import uk.gov.hmrc.http.HttpReads.Implicits._
 
  @Singleton
- class ApiDefinitionConnector @Inject()(  
+ class ApiDefinitionConnector @Inject()(
    val http: HttpClient with WSGet,
    val config: Config)(implicit val ec: ExecutionContext) extends Logging with ApiDefinitionJsonFormatters {
 
     private def definitionUrl(serviceBaseUrl: String, serviceName: String) =
       s"$serviceBaseUrl/api-definition/$serviceName"
 
-    def getDefinitionByServiceName(serviceName: String)(implicit hc: HeaderCarrier): Future[Option[ApiDefinition]] = {
+    def getDefinitionByServiceName(serviceName: String)(implicit hc: HeaderCarrier): Future[Either[Throwable, ApiDefinition]] = {
       logger.info(s"${this.getClass.getSimpleName} - fetchApiDefinition")
-      val r = http.GET[Option[ApiDefinition]](definitionUrl(config.baseUrl, serviceName))
+      val r = http.GET[Option[ApiDefinition]](definitionUrl(config.baseUrl, serviceName)).map{
+        case Some(x) => Right(x)
+        case _ => Left(new NotFoundException(" unable to fetch definition"))
+      }
 
       r.recover {
         case NonFatal(e)          =>
           logger.error(s"Failed $e")
-          throw e
+         Left(e)
       }
     }
   
--- app/uk/gov/hmrc/apiplatformapicataloguepublish/apidefinition/service/ApiDefinitionService.scala
+++ app/uk/gov/hmrc/apiplatformapicataloguepublish/apidefinition/service/ApiDefinitionService.scala
@@ -16,34 +16,43 @@
 
 package uk.gov.hmrc.apiplatformapicataloguepublish.apidefinition.service
 
-import uk.gov.hmrc.apiplatformapicataloguepublish.apidefinition.connector.ApiDefinitionConnector
+import uk.gov.hmrc.apiplatformapicataloguepublish.apidefinition.connector.{ApiDefinitionConnector, ApiProducerTeamConnector}
 import uk.gov.hmrc.apiplatformapicataloguepublish.apidefinition.models.ApiDefinition
+
 import javax.inject.{Inject, Singleton}
 import uk.gov.hmrc.http.HeaderCarrier
+
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext
 
 @Singleton()
-class ApiDefinitionService @Inject() (apiDefinitionConnector: ApiDefinitionConnector)(implicit val ec: ExecutionContext) {
+class ApiDefinitionService @Inject() (apiDefinitionConnector: ApiDefinitionConnector,
+                                      apiProducerTeamConnector: ApiProducerTeamConnector)(implicit val ec: ExecutionContext) {
 
-  def getDefinitionByServiceName(serviceName: String)(implicit hc: HeaderCarrier): Future[Option[String]] = {
-    apiDefinitionConnector.getDefinitionByServiceName(serviceName).map(maybeApiDefinition =>
-      maybeApiDefinition
-      .map(x => s"${getBaseUrl(x)}/api/conf/${getLatestVersion(x)}/application.raml")
+  def getDefinitionByServiceName(serviceName: String)(implicit hc: HeaderCarrier) = {
+    apiDefinitionConnector.getDefinitionByServiceName(serviceName)
+      .map(maybeApiDefinition =>
+      maybeApiDefinition.map(getRamlString)
     )
     //get raml from api producer microservice (how do we determine the link for this?)
     // convert raml to OAS and add our api catalogue specific items.
     // publish api on api catalogue
   }
 
+  private def getRamlString(apiDefinition: ApiDefinition)(implicit hc:HeaderCarrier)= {
+    apiProducerTeamConnector
+      .getApiRamlString(getBaseUrl(apiDefinition), s"/api/conf/${getLatestVersion(apiDefinition)}/application.raml")
+
+  }
+
   
-  private def getBaseUrl(apiDefintion: ApiDefinition): String = {
-    //apiDefintion.serviceBaseUrl
+  private def getBaseUrl(apiDefinition: ApiDefinition): String = {
+    //apiDefinition.serviceBaseUrl
     s"http://localhost:9820" // customs-declarations running locally
   }
 
-  private def getLatestVersion(apiDefintion: ApiDefinition): String = {
-    //apiDefintion.versions.headOption.map(apiVersionDefinition => apiVersionDefinition.version.value).getOrElse("1.0")
+  private def getLatestVersion(apiDefinition: ApiDefinition): String = {
+    //apiDefinition.versions.headOption.map(apiVersionDefinition => apiVersionDefinition.version.value).getOrElse("1.0")
     "1.0"
 
   }
