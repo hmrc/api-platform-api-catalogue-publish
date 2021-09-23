3bd5607afb55de8abf6d9b06e5d247e670d9660f
Matt Clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2021-09-23 14:09:22 +0100

    API-5106: updated jackson library versions to 2.11.1

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
 
   lazy val scalaCheckVersion = "1.14.0"
   lazy val enumeratumVersion = "1.6.2"
-  lazy val jacksonVersion = "2.10.5"
+  lazy val jacksonVersion = "2.11.1"
 
   val compile = Seq(
     "uk.gov.hmrc"                       %% "bootstrap-backend-play-28"      % "5.14.0",
@@ -18,6 +18,7 @@ object AppDependencies {
     "org.raml"                          % "webapi-parser"                   % "0.5.0",
     "org.typelevel"                     %% "cats-core"                      % "2.4.2",
     "io.swagger.parser.v3"              % "swagger-parser-v3"               % "2.0.24",
+    "com.fasterxml.jackson.module"        %% "jackson-module-scala"            % jacksonVersion,
     "com.fasterxml.jackson.core"        % "jackson-annotations"             % jacksonVersion,
     "com.fasterxml.jackson.core"        % "jackson-databind"                % jacksonVersion,
     "com.fasterxml.jackson.core"        %  "jackson-core"                   % jacksonVersion,
