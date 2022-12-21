a7e0c2b101de98026fbbf8e32e8093650c0ce75f
Anjum Abbas <49648217+anjumabbas5@users.noreply.github.com>
2022-12-21 16:23:14 +0000

    API-5589 - Bootstrap Play 7.12.0 and sbt 2.8.18 upgrade. (#39)
    * API-5589 - Bootstrap Play 7.12.0 and sbt 2.8.18 upgrade.
    
    * API-5589 - PR bot comments - upgraded sbt version to 1.6.2
    
    * API-5589 - PR bot comments - updated application-json-logger.xml
    
    * API-5589 - PR bot comments - updated AuditModule to uk.gov.hmrc.play.audit.AuditModule
    
    * API-5589 - PR bot comments - updated Readme
    
    * API-5589 - fixed build.sbt warnings
    
    * API-5589 - tidy up
    
    * API-5589: Bumped swagger parser and jacksons versions
    
    * API-5589: Fixed failing tests
    
    * API-5589: Fixed typo
    
    Co-authored-by: syed-ali-hmrc <al1.sy3d@outlook.com>

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 
 # api-platform-api-catalogue-publish
 
-This is a placeholder README.md for a new repository
+The api-platform-api-catalogue-publish publishes APIs to the [API Catalogue](https://admin.tax.service.gov.uk/api-catalogue).
 
-### License
+## POST /publish/:serviceName endpoint
 
-This code is open source software licensed under the [Apache 2.0 License]("http://www.apache.org/licenses/LICENSE-2.0.html").
+The [deploy-microservice job in Jenkins](https://orchestrator.tools.production.tax.service.gov.uk/job/deploy-microservice/) runs the
+[publish API script](https://github.com/hmrc/api-platform-scripts/blob/main/publish_api.py)
+which calls POST /publish/:serviceName endpoint on this service to publish the API onto the [API Catalogue](https://admin.tax.service.gov.uk/api-catalogue).
+
+This is also called by clicking on the Publish button on [this hidden page](https://admin.qa.tax.service.gov.uk/api-gatekeeper/apicatalogue/start) in api-gatekeeper-frontend. 
+
+## POST /publish-all endpoint
+This is called by clicking on the Publish All Apis button on [this hidden page](https://admin.qa.tax.service.gov.uk/api-gatekeeper/apicatalogue/start) in api-gatekeeper-frontend.
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,28 +8,37 @@ object AppDependencies {
 
   lazy val scalaCheckVersion = "1.14.0"
   lazy val enumeratumVersion = "1.6.3"
-  lazy val jacksonVersion = "2.11.1"
+  lazy val jacksonVersion = "2.12.6"
+  lazy val bootstrapVersion = "7.12.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"                       %% "bootstrap-backend-play-28"      % "5.14.0",
+    "uk.gov.hmrc"                       %% "bootstrap-backend-play-28"      % bootstrapVersion,
     "com.typesafe.play"                 %% "play-json"                      % "2.9.2",
     "com.typesafe.play"                 %% "play-json-joda"                 % "2.9.2",
     "com.beachape"                      %% "enumeratum-play-json"           % enumeratumVersion,
     "org.raml"                          % "webapi-parser"                   % "0.5.0",
     "org.typelevel"                     %% "cats-core"                      % "2.4.2",
-    "io.swagger.parser.v3"              % "swagger-parser-v3"               % "2.0.24",
+    "io.swagger.parser.v3"              % "swagger-parser"                  % "2.1.9"
+      excludeAll(
+      ExclusionRule("com.fasterxml.jackson.core", "jackson-databind"),
+      ExclusionRule("com.fasterxml.jackson.core", "jackson-core"),
+      ExclusionRule("com.fasterxml.jackson.core", "jackson-annotations"),
+      ExclusionRule("com.fasterxml.jackson.dataformat", "jackson-dataformat-yaml"),
+      ExclusionRule("com.fasterxml.jackson.datatype", "jackson-datatype-jsr310")
+    ),
     "com.fasterxml.jackson.module"      %% "jackson-module-scala"           % jacksonVersion,
     "com.fasterxml.jackson.core"        % "jackson-annotations"             % jacksonVersion,
     "com.fasterxml.jackson.core"        % "jackson-databind"                % jacksonVersion,
     "com.fasterxml.jackson.core"        % "jackson-core"                    % jacksonVersion,
     "com.fasterxml.jackson.dataformat"  % "jackson-dataformat-yaml"         % jacksonVersion,
+    "com.fasterxml.jackson.datatype"    % "jackson-datatype-jsr310"         % jacksonVersion,
     "org.apache.httpcomponents"         % "httpclient"                      % "4.3.1",
     "org.apache.httpcomponents"         % "httpmime"                        % "4.3.1"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"                 %% "bootstrap-test-play-28"             % "5.14.0"      % "test, it",
-    "org.mockito"                 %% "mockito-scala-scalatest"            % "1.16.42"     % "test, it",
-    "com.github.tomakehurst"      %  "wiremock-jre8-standalone"           % "2.27.1"      % "test, it"
+    "uk.gov.hmrc"                 %% "bootstrap-test-play-28"             % bootstrapVersion    % "test, it",
+    "org.mockito"                 %% "mockito-scala-scalatest"            % "1.16.42"           % "test, it",
+    "com.github.tomakehurst"      %  "wiremock-jre8-standalone"           % "2.27.1"            % "test, it"
   )
 }
